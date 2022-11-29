35a9424541625bf0f4475b9d320684e79db53030
Tom <33316690+tomsong00@users.noreply.github.com>
2022-11-29 10:40:59 +0800

    Update and rename dataset_generator - 副本.py to dataset_generator.py

--- dataset_generator.py
+++ dataset_generator.py
@@ -1,17 +1,13 @@
 import numpy as np
-# 生成数据集，共九种组合
-num_jobs_list = [10, 20, 30]
-num_machines_list = [4, 6, 8]
-
-
+num_jobs_list = [100, 200, 300]
+num_machines_list = [2, 3, 4]
 for num_jobs in num_jobs_list:
     for num_machines in num_machines_list:
         with open("./dataset/{}-{}.txt".format(num_jobs, num_machines), "w") as f:
-            # 写入工件数和机器数
             lines = ["{} {}\n".format(num_jobs, num_machines)]
             for i in range(num_jobs):
                 line = ""
                 for j in range(num_machines):
-                    line += " " + " "  # 写入工件对应机器的耗时，随机生成
+                    line += " " + " "  
                 lines.append(line+"\n")
             f.writelines(lines)
